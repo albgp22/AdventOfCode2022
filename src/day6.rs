8b9958c6b3be8919108d06f2c59f79b9e24068bc
Alberto <albgp22@gmail.com>
2022-12-06 15:50:35 +0100

    Refactor Day 6

--- src/day6.rs
+++ src/day6.rs
@@ -2,33 +2,24 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::collections::HashSet;
 
-pub fn solve(reader: BufReader<File>) {
-    let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
-    let line0 = &lines[0];
-    let chars: Vec<char> = line0.chars().into_iter().collect();
+fn solve_for_n(n: usize, line: &str) -> usize{
     let mut res: usize = 0;
-    for i in 0..(chars.len()-4){
+    let chars: Vec<char> = line.chars().into_iter().collect();
+    for i in 0..(chars.len()-n){
         let mut last_chars = HashSet::new();
-        for _ in i..(i+4){
+        for j in i..(i+n){
             last_chars.insert(chars[j]);
         }
-        if last_chars.len()==4{
-            res=i+4;
+        if last_chars.len()==n{
+            res=i+n;
             break
         }
     }
-    println!("{}", res);
+    return res;
+}
 
-    let mut res: usize = 0;
-    for i in 0..(chars.len()-14){
-        let mut last_chars = HashSet::new();
-        for _ in i..(i+14){
-            last_chars.insert(chars[j]);
-        }
-        if last_chars.len()==14{
-            res=i+14;
-            break
-        }
-    }
-    println!("{}", res);
+pub fn solve(reader: BufReader<File>) {
+    let line: String = reader.lines().next().unwrap().unwrap();
+    println!("{}", solve_for_n(4, &line));
+    println!("{}", solve_for_n(14, &line));
 }
